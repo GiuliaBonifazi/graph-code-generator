5c290e73fb8cd91665832d918ccc81f323318528
GiuliaBonifazi <giulia.bonifazi@studio.unibo.it>
2025-08-08 16:12:32 +0200

    working dropdown menu

--- src/pages/GraphOptions.jsx
+++ src/pages/GraphOptions.jsx
@@ -3,11 +3,13 @@ import GenericInput from "./components/GenericInput"
 import PickedColour from "./components/PickedColour"
 import { useState } from "react"
 import GenericButton from "./components/GenericButton"
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react"
 
 
 const GraphOptions = () => {
     const [currColor, setCurrColor] = useState("#000000")
     const [pickedColors, setPickedColors] = useState([])
+    const [graphType, setGraphType] = useState(null)
 
     const onConfirmPickedColor = (event) => {
         if (currColor != null && !pickedColors.includes(currColor)) {
@@ -27,7 +29,27 @@ const GraphOptions = () => {
     return <>
         <Title title="Graph options"></Title>
         {/* TODO DATA PREVIEW */}
-        
+        <div className="flex justify-center items-center">
+            <Menu as="div">
+                <MenuButton className="genericButton border rounded h-fit p-2 w-48 text-center">
+                    {graphType == null ? "Pick graph type" : graphType}
+                </MenuButton>
+
+                <MenuItems className="flex flex-col space-y-2">
+                    {
+                        ["Line graph", "Bar graph", "Pie chart"].map((element) => {
+                            return <MenuItem >
+                                <a
+                                className="genericButton border rounded h-fit p-2 w-24 text-center"
+                                onClick={() => setGraphType(element)}>
+                                    {element}
+                                </a>
+                            </MenuItem>
+                        }) 
+                    }
+                </MenuItems>
+            </Menu>
+        </div>
         <form className="flex flex-col justify-start space-y-4 pl-[10rem] pr-0">
             <GenericInput labelText="X axis data" id="x-axis-data" type="text"></GenericInput>
             <GenericInput labelText="Y axis data" id="y-axis-data" type="text"></GenericInput>
