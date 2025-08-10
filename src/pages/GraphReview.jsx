30f8b8e6e904e2da873854d90418df01262ef40a
GiuliaBonifazi <giulia.bonifazi@studio.unibo.it>
2025-08-10 14:27:05 +0200

    made nav bar responisve

--- src/pages/GraphReview.jsx
+++ src/pages/GraphReview.jsx
@@ -4,28 +4,29 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 const GraphReview = () => {
     return <>
         <Title title="Graph review"></Title>
-        <div className="mx-8 lg:mx-40 flex items-center flex-col space-y-2">
-            <h2 className="text-left self-start">Your graph code</h2>
-            <TabGroup className="w-full">
-                <TabList className="space-x-2">
-                    <Tab className="rounded border genericButton w-fit h-fit p-2">JavaScript</Tab>
-                    <Tab className="rounded border genericButton w-fit h-fit p-2">Python</Tab>
-                </TabList>
-                <TabPanels className="border rounded w-full min-h-[40rem] min-w-[20rem] pasteArea">
-                    <TabPanel>
-                        <pre><code>console.log("Here's your JS code!")</code></pre>
-                    </TabPanel>
-                    <TabPanel>
-                        <pre><code>print("Here's your Python code!")</code></pre>
-                    </TabPanel>
-                </TabPanels>
-            </TabGroup>
-            <div className="flex flex-col w-full">
-                <h2 className="text-left">Used prompt:</h2>
-                <textarea className="pasteArea border rounded resize-y min-w-[20rem] h-40" placeholder="Prompt goes here..."></textarea>
+        <div className="w-full h-full flex flex-row items-center justify-center">
+            <div className="mx-4 lg:mx-8 flex w-full items-center flex-col space-y-2">
+                <h2 className="text-left self-start">Your graph code</h2>
+                <TabGroup className="w-full">
+                    <TabList className="space-x-2">
+                        <Tab className="rounded border genericButton w-fit h-fit p-2">JavaScript</Tab>
+                        <Tab className="rounded border genericButton w-fit h-fit p-2">Python</Tab>
+                    </TabList>
+                    <TabPanels className="border rounded w-full min-h-[40rem] min-w-[20rem] pasteArea">
+                        <TabPanel>
+                            <pre><code>console.log("Here's your JS code!")</code></pre>
+                        </TabPanel>
+                        <TabPanel>
+                            <pre><code>print("Here's your Python code!")</code></pre>
+                        </TabPanel>
+                    </TabPanels>
+                </TabGroup>
+                <div className="flex flex-col w-full">
+                    <h2 className="text-left">Used prompt:</h2>
+                    <textarea className="pasteArea border rounded resize-y min-w-[20rem] h-40" placeholder="Prompt goes here..."></textarea>
+                </div>
             </div>
         </div>
-
     </>
 }
 
--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 
 function Navbar() {
-    return <Disclosure as="nav" className="lg:flex lg:p-4 lg:h-24 lg:items-center font-bold border-b mb-4">
+    return <Disclosure as="nav" className="flex p-2 lg:p-4 lg:h-24 items-center font-bold border-b mb-4">
         Welcome to the graph generator!
-        <div className="lg:h-full lg:w-full lg:flex lg:content-center lg:justify-end ">
+        <div className="h-full w-full flex content-center justify-end ">
             <Link to="/" className="genericButton border rounded h-fit w-fit p-2 self-end">
                 Choose new dataset
             </Link>
