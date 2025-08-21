8ae88ab69df7d3632e4f679c0476d24d494764fa
GiuliaBonifazi <giulia.bonifazi@studio.unibo.it>
2025-08-21 15:09:16 +0200

    finished unstyled table

--- src/components/table/DataTable.jsx
+++ src/components/table/DataTable.jsx
@@ -1,30 +1,52 @@
 import useGraphFormContext from "../../hooks/useGraphFormContext"
-import TableHeaders from "./TableHeaders"
-import TableRow from "./TableRow"
+import { flexRender, useReactTable, getCoreRowModel, } from "@tanstack/react-table"
 
 function DataTable() {
     const {options} = useGraphFormContext()
-
     if (!options) {
         return <p>No data</p>
     }
 
     const rows = options.uploadData.rows
     const headers = options.uploadData.headers
-
     if (!rows || !headers) {
         return <p>No data</p>
     }
 
+    const table = useReactTable({
+        data: rows,
+        columns: headers,
+        getCoreRowModel: getCoreRowModel(),
+    })
     return <>
-        <table>
+        <table className="border rounded ">
             <caption>Your data</caption>
-            <TableHeaders headers={headers}></TableHeaders>
-            {
-                rows.map(row => {
-                    return <TableRow row={row}></TableRow>
-                })
-            }
+            <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                            <th className="border text-left text-sm font-semibold w-fit p-2"
+                                key={header.id} colSpan="col">
+                                {
+                                    flexRender(header.column.columnDef.header, header.getContext())
+                                }
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                        <td className="border w-fit p-2 text-center"
+                            key={cell.id}>
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
         </table>
     </>
 }
