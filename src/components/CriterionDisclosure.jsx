8bd1b04c3d8866cba2281865679cafaf61947fe7
GiuliaBonifazi <giulia.bonifazi@studio.unibo.it>
2025-08-22 18:23:25 +0200

    added checkbox to criteria display

--- src/components/CriterionDisclosure.jsx
+++ src/components/CriterionDisclosure.jsx
@@ -18,7 +18,10 @@ function CriterionDisclosure({level, id, name, desc} = props) {
       break;
   }
   return <Disclosure key={id + "-disclosure"} as="div" className="flex flex-col text-left">
-    <DisclosureButton key={id + "-button"} className={"p-2 text-left hover:border " + levelClass + "Background"}>{name}</DisclosureButton>
+    <div className={'flex flex-row ' + levelClass + "Background w-full"}>
+      <DisclosureButton key={id + "-button"} className={"p-2 text-left hover:border "}>{name}</DisclosureButton>
+      <input type="checkbox" dims="w-fit"></input>
+    </div>
     <DisclosurePanel key={id + "-panel"} className={"text-left p-2 "  + levelClass + "Highlight"}>
       {desc}
     </DisclosurePanel>
