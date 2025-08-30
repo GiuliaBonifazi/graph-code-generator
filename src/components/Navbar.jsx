4d66c4cc7d13b1759f5c534dcacdf9699f965d25
GiuliaBonifazi <giulia.bonifazi@studio.unibo.it>
2025-08-30 19:29:19 +0200

    added report stats button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,11 @@ function Navbar(props) {
     return <Disclosure as="nav" className="flex p-2 lg:p-4 lg:h-24 items-center font-bold border-b mb-4">
         Welcome to the graph generator!
         <div className="h-full w-full flex flex-row content-center justify-end space-x-4">
-            <Link to="/" className="genericButton border rounded h-fit w-fit p-2 self-end">
+            <Link to="/" className="genericButton border rounded h-fit w-fit p-2 self-end text-center">
                 Choose new dataset
+            </Link>
+            <Link to="/" className="genericButton border rounded h-fit w-fit p-2 self-end text-center">
+                See report stats
             </Link>
         </div>
     </Disclosure>
